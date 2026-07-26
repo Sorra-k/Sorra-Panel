baa0018084c5d7e865f1bd2f8f74462fe3458bee
Sorra-k <biolemon12@gmail.com>
2026-07-26 15:40:56 +0200

    Update app.js

--- app.js
+++ app.js
@@ -145,19 +145,11 @@ function toggleLike(postId, currentLikes) {
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyB1xGcKVI4Ty23cVDY8CHYRi6AEk-Sxj3U",
+ const firebaseConfig = {
+    apiKey: "AIzaSy...",
     authDomain: "sorra-works.firebaseapp.com",
     projectId: "sorra-works",
-    storageBucket: "sorra-works.firebasestorage.app",
-    messagingSenderId: "646064583120",
-    appId: "1:646064583120:web:7d3f8728f9476f800849d5",
-    measurementId: "G-JRBM2MQR9C"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
+    storageBucket: "sorra-works.appspot.com",
+    messagingSenderId: "123456789",
+    appId: "1:123456789:web:abcdef..."
+};
